d7aa9e583da7958a02528b96aa98e0950f7eba2c
Raymond Wongso <raymondwongso.work@gmail.com>
2023-02-03 11:11:22 +0700

    Tidying some code in log package and add context log

--- log/logger.go
+++ log/logger.go
@@ -1,7 +1,5 @@
 package log
 
-import "golang.org/x/exp/maps"
-
 // Logger defines generic interface for all logger.
 type Logger interface {
 	Trace(msg string, args ...interface{})
@@ -18,15 +16,4 @@ type Logger interface {
 	Fatalw(msg string, md Metadata, args ...interface{})
 	Panic(msg string, args ...interface{})
 	Panicw(msg string, md Metadata, args ...interface{})
-}
-
-// Metadata defines metadata for logger, which will be included whenever log is written.
-type Metadata map[string]interface{}
-
-// MergeMetadata creates new metadata as a result from merging between md1 and md2.
-func MergeMetadata(md1, md2 Metadata) Metadata {
-	res := Metadata{}
-	maps.Copy(res, md1)
-	maps.Copy(res, md2)
-	return res
 }
